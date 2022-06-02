3a837c8adfadc2ced454de19c37d3278ea899d11
Xuezhou Wen <xuezhou1998@gmail.com>
2022-06-02 12:37:30 -0700

    Initial Commit

---
+++ my_login/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  my_login
+//
+//  Created by Xuezhuo Wen on 6/2/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
